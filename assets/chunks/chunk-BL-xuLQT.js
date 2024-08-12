1d1cdf2d8b6e4659f2e25693390e92c7644796db
EralChen <82485978+EralChen@users.noreply.github.com>
2024-08-12 07:35:04 +0000

    website deploy

---
+++ assets/chunks/chunk-BL-xuLQT.js
@@ -0,0 +1,5 @@
+import{l as i}from"./chunk-8hXFf3E9.js";import{T as c}from"./chunk-BIbgiuQN.js";import{d as g,K as m,L as u,o as p,c as S,h as r,w as n,g as l,b as s}from"./chunk-m6PLwqMy.js";import"./chunk-CJMEDYvw.js";const f={id:"loadStyleStringDemoNode","sk-flex":"row-center2"},h=g({__name:"basic",setup(_){class a extends c{color="red";get cssRule(){return`
+    #loadStyleStringDemoNode{
+      background: ${this.color};
+    }
+    `}constructor(e){super(),this.color=e}add(e){e&&(this.color=e),this.removeHandler=i(this.cssRule)}}const o=new a("red");return o.add(),m(()=>{o.remove()}),(d,e)=>{const t=u("el-button");return p(),S("div",f,[r(t,{onClick:e[0]||(e[0]=()=>s(o).toggle())},{default:n(()=>[l(" toggleLoadStyleStringDemoNodeBg ")]),_:1}),r(t,{onClick:e[1]||(e[1]=()=>s(o).reset("blue"))},{default:n(()=>[l(" blue ")]),_:1})])}}});export{h as default};
