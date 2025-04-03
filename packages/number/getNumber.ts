d862cf6daaed84df52c99812371700efabcca6f7
EralChen <eralchen@foxmail.com>
2025-04-03 16:43:11 +0800

    fix: getPartialNumber

--- packages/number/getNumber.ts
+++ packages/number/getNumber.ts
@@ -2,5 +2,7 @@
  * 尝试获取数字, 如果获取失败, 则返回默认值
  */
 export function getNumber (value, defaultValue = 0) {
-  return Number.isNaN(value) ? defaultValue : Number(value)
+  return Number.isNaN(Number(value))
+    ? defaultValue
+    : Number(value)
 }
--- packages/number/getPartialNumber.ts
+++ packages/number/getPartialNumber.ts
@@ -4,5 +4,7 @@ import { getNumber } from './getNumber'
  * 尝试获取数字, 如果获取失败, 则返回 undefined
  */
 export function getPartialNumber (value): number | undefined {
-  return Number.isNaN(value) ? undefined : Number(value)
+  return Number.isNaN(Number(value))
+    ? undefined
+    : Number(value)
 }
